972587171d1758ce5602a32b09aac991f7a13584
Prasath-Arumugam <prasatharumugam040@gmail.com>
2026-08-22 17:04:08 +0530

    Completed the portfolio project setup

---
+++ personal_portfolio/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    
+    <div className="min-h-screen flex items-center justify-center bg-blue-500">
+      <h1 className="text-5xl font-bold text-white">
+
+        Tailwind CSS Works!
+
+      </h1>
+
+    </div>
+    
+    </>
+  )
+}
+
+export default App
